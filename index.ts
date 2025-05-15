15a2c783c7ae52010f03391e35b4898f0c095cca
Arttu Hanska <arttu.hanska@gmail.com>
2025-05-15 14:04:40 +0300

    Add working import example

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import typescriptEslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import { workingImport } from "./workingImport";
 
 console.log(typescriptEslint);
 console.log(defineConfig);
-
+console.log(workingImport);
---
+++ workingImport.ts
@@ -0,0 +1,3 @@
+export const workingImport = () => {
+  console.log("This import works fine");
+};
